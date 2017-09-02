bce0192e124f5c22f47f66e2e7be83f3e967a1a5
Tristan <tris790@gmail.com>
2017-09-02 18:49:36 -0400

    Added material design and made basic layout

--- public/js/twitter.js
+++ public/js/twitter.js
@@ -1,8 +1,30 @@
-const fetchTweets = () => {
-  fetch(
-    "https://twitter.com/realDonaldTrump",
-    { mode: "no-cors" },
-    d => console.log("request to twitter sent successfuly!"),
-    err => console.log(err)
-  );
+const fetchTweets = async () => {
+  const res = await fetch("/fetch?count=5");
+  return (parsedRes = await res.json());
+};
+
+const createCardFromTweet = tweet => {
+  return `<div class="mdc-card mdc-card--with-avatar">
+  <section class="mdc-card__primary">
+    <img class="mdc-car__avatar" src="${tweet.user.profile_image_url_https}"/>
+    <h1 class="mdc-card__title mdc-card__title--large">${tweet.user
+      .name} @${tweet.user.screen_name} ${tweet.created_at}</h1>
+    <h2 class="mdc-card__subtitle"></h2>
+  </section>
+  <section class="mdc-card__supporting-text">
+    ${tweet.text}
+  </section>
+  <section class="mdc-card__actions">
+    <button class="mdc-button mdc-button--compact mdc-card__action">Open</button>
+    <button class="mdc-button mdc-button--compact mdc-card__action">Like</button>
+  </section>
+</div>`;
+};
+
+const loadTweets = () => {
+  fetchTweets().then(data => {
+    console.log(data);
+    const tweetResult = data.content.map(tweet => createCardFromTweet(tweet));
+    document.getElementById("tweets").innerHTML = tweetResult.join("");
+  });
 };
